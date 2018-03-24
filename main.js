31e3d837c4dc978dc19a3a8ed64a406d272824bc
Jason Yang <yangjason445@gmail.com>
2018-03-24 22:27:48 +0000

    nineties website

---
+++ main.js
@@ -0,0 +1,18 @@
+function harold_img(src, width, height, alt) {
+    var harold = document.createElement("img");
+    harold.src = src;
+    harold.width = width;
+    harold.height = height;
+    harold.alt = alt;
+    
+    document.body.appendChild(harold);
+}
+function harold2_img(src,width,height,alt){
+    var harold2 = document.createElement("img");
+    harold2.src = src;
+    harold2.width = width;
+    harold2.height = height;
+    harold2.alt = alt;
+    
+    document.body.appendChild(harold2);
+}
